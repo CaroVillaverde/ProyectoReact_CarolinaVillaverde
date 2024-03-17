e5a9398b11280978975f92a2d792f3f32d484ace
Carolina Villaverde <villaverdecb@gmail.com>
2024-03-17 06:24:07 -0300

    Proyecto final

---
+++ src/components/ItemList.js
@@ -0,0 +1,11 @@
+
+
+const ItemList =({products}) => {
+    return(
+        <div>
+            {products.map(prod => <Item key={prod.id} {..prod} />)}
+        </div>
+    )
+}
+
+export default ItemList;
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,7 +1,23 @@
+import { useState,useEffect} from "react";
+import { getProducts } from "../components/asyncMock";
+
+
 const ItemListContainer = ({greeting}) => {
+    const[products, getProducts] = useState([])
+    
+    useEffect(() => {
+        getProducts()
+        .them(response => {
+            getProducts(response)
+        })
+        .catch(error => {
+            console.error(error)
+        })
+    })
     return(
         <div>
             <h1>{greeting}</h1>
+            <itemList products={products}/>       
         </div>
     )
 }
